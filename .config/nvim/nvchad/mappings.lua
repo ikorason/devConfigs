6989d925f06275497c46338eebef7d584f41bdb5
ikorason <irfanabliz914@gmail.com>
2023-11-09 18:46:53 -0500

    chore: add copilot

--- .config/nvim/nvchad/mappings.lua
+++ .config/nvim/nvchad/mappings.lua
@@ -2,17 +2,26 @@
 local M = {}
 
 M.general = {
-  n = {
-    [";"] = { ":", "enter command mode", opts = { nowait = true } },
-    ["Q"] = { "<nop>", "NADA" },
-  },
-  v = {
-    ["K"] = { ":m '<-2<CR>gv=gv'", "Keep selection, move up" },
-    ["J"] = { ":m '>+1<CR>gv=gv'", "Keep selection, move down" }
-  },
-  x = {
-    ["<leader>p"] = { "\"_dP" }
-  }
+	n = {
+		[";"] = { ":", "enter command mode", opts = { nowait = true } },
+		["Q"] = { "<nop>", "NADA" },
+	},
+	v = {
+		["K"] = { ":m '<-2<CR>gv=gv'", "Keep selection, move up" },
+		["J"] = { ":m '>+1<CR>gv=gv'", "Keep selection, move down" },
+	},
+	x = {
+		["<leader>p"] = { '"_dP' },
+	},
+	i = {
+		["<C-l>"] = {
+			function()
+				vim.fn.feedkeys(vim.fn["copilot#Accept"](), "")
+			end,
+			"Copilot Accept",
+			{ replace_keycodes = true, nowait = true, silent = true, expr = true, noremap = true },
+		},
+	},
 }
 
 -- more keybinds!
--- .config/nvim/nvchad/plugins.lua
+++ .config/nvim/nvchad/plugins.lua
@@ -3,81 +3,94 @@ local overrides = require("custom.configs.overrides")
 ---@type NvPluginSpec[]
 local plugins = {
 
-  -- Override plugin definition options
+	-- Override plugin definition options
 
-  {
-    "neovim/nvim-lspconfig",
-    event = "VeryLazy",
-    dependencies = {
-      -- format & linting
-      {
-        "jose-elias-alvarez/null-ls.nvim",
-        config = function()
-          require "custom.configs.null-ls"
-        end,
-      },
-    },
-    config = function()
-      require "plugins.configs.lspconfig"
-      require "custom.configs.lspconfig"
-    end, -- Override to setup mason-lspconfig
-  },
+	{
+		"neovim/nvim-lspconfig",
+		event = "VeryLazy",
+		dependencies = {
+			-- format & linting
+			{
+				"jose-elias-alvarez/null-ls.nvim",
+				config = function()
+					require("custom.configs.null-ls")
+				end,
+			},
+		},
+		config = function()
+			require("plugins.configs.lspconfig")
+			require("custom.configs.lspconfig")
+		end, -- Override to setup mason-lspconfig
+	},
 
-  -- override plugin configs
-  {
-    "williamboman/mason.nvim",
-    opts = overrides.mason
-  },
+	-- override plugin configs
+	{
+		"williamboman/mason.nvim",
+		opts = overrides.mason,
+	},
 
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = overrides.treesitter,
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = overrides.treesitter,
+	},
 
-  {
-    "nvim-tree/nvim-tree.lua",
-    opts = overrides.nvimtree,
-  },
+	{
+		"nvim-tree/nvim-tree.lua",
+		opts = overrides.nvimtree,
+	},
 
-  -- Install a plugin
-  {
-    "max397574/better-escape.nvim",
-    event = "InsertEnter",
-    config = function()
-      require("better_escape").setup()
-    end,
-  },
+	-- Install a plugin
+	{
+		"max397574/better-escape.nvim",
+		event = "InsertEnter",
+		config = function()
+			require("better_escape").setup()
+		end,
+	},
 
-  {
-    'wfxr/minimap.vim',
-    lazy = false,
-    build = "cargo install --locked code-minimap",
-    cmd = { "Minimap", "MinimapClose", "MinimapToggle", "MinimapRefresh", "MinimapUpdateHighlight" },
-    -- config = function()
-    --     vim.cmd("let g:minimap_width = 10")
-    --     vim.cmd("let g:minimap_auto_start = 1")
-    --     vim.cmd("let g:minimap_auto_start_win_enter = 1")
-    -- end,
-    -- init = function()
-    --   vim.g.minimap_width = 10
-    --   vim.g.minimap_auto_start = 1
-    --   vim.g.minimap_auto_start_win_enter = 1
-    -- end
-  },
+	{
+		"github/copilot.vim",
+		lazy = false,
+		config = function()
+			-- Mapping tab is already used by NvChad
+			vim.g.copilot_no_tab_map = true
+			vim.g.copilot_assume_mapped = true
+			vim.g.copilot_tab_fallback = ""
+			-- The mapping is set to other key, see custom/lua/mappings
+			-- or run <leader>ch to see copilot mapping section
+		end,
+	},
 
-  -- To make a plugin not be loaded
-  -- {
-  --   "NvChad/nvim-colorizer.lua",
-  --   enabled = false
-  -- },
+	{
+		"wfxr/minimap.vim",
+		lazy = false,
+		build = "cargo install --locked code-minimap",
+		cmd = { "Minimap", "MinimapClose", "MinimapToggle", "MinimapRefresh", "MinimapUpdateHighlight" },
+		-- config = function()
+		--     vim.cmd("let g:minimap_width = 10")
+		--     vim.cmd("let g:minimap_auto_start = 1")
+		--     vim.cmd("let g:minimap_auto_start_win_enter = 1")
+		-- end,
+		-- init = function()
+		--   vim.g.minimap_width = 10
+		--   vim.g.minimap_auto_start = 1
+		--   vim.g.minimap_auto_start_win_enter = 1
+		-- end
+	},
 
-  -- All NvChad plugins are lazy-loaded by default
-  -- For a plugin to be loaded, you will need to set either `ft`, `cmd`, `keys`, `event`, or set `lazy = false`
-  -- If you want a plugin to load on startup, add `lazy = false` to a plugin spec, for example
-  -- {
-  --   "mg979/vim-visual-multi",
-  --   lazy = false,
-  -- }
+	-- To make a plugin not be loaded
+	-- {
+	--   "NvChad/nvim-colorizer.lua",
+	--   enabled = false
+	-- },
+
+	-- All NvChad plugins are lazy-loaded by default
+	-- For a plugin to be loaded, you will need to set either `ft`, `cmd`, `keys`, `event`, or set `lazy = false`
+	-- If you want a plugin to load on startup, add `lazy = false` to a plugin spec, for example
+	-- {
+	--   "mg979/vim-visual-multi",
+	--   lazy = false,
+	-- }
 }
 
 return plugins
