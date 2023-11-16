1fb8e9a94e9a49bb3a2da35987463431bf4892ab
ikorason <irfanabliz914@gmail.com>
2023-11-16 11:35:21 -0500

    update

---
+++ .config/nvim/lua/plugins/editor.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    enabled = false,
+    "folke/flash.nvim",
+    ---@type Flash.Config
+    opts = {
+      search = {
+        forward = true,
+        multi_window = false,
+        wrap = false,
+        incremental = true,
+      },
+    },
+  },
+
+  {
+    "dinhhuy258/git.nvim",
+    event = "BufReadPre",
+    opts = {
+      keymaps = {
+        -- Open blame window
+        blame = "<Leader>gb",
+        -- Open file/folder in git repository
+        browse = "<Leader>go",
+      },
+    },
+  },
+}
