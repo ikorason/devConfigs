2b52b212966edd0413fd19fcdf8cb3b85a1b69a7
ikorason <irfanabliz914@gmail.com>
2023-11-16 13:29:31 -0500

    animation

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -1,100 +1,113 @@
 return {
-  -- messages, cmdline and the popupmenu
-  "folke/noice.nvim",
-  opts = function(_, opts)
-    table.insert(opts.routes, {
-      filter = {
-        event = "notify",
-        find = "No information available",
-      },
-      opts = { skip = true },
-    })
+  {
+    -- messages, cmdline and the popupmenu
+    "folke/noice.nvim",
+    opts = function(_, opts)
+      table.insert(opts.routes, {
+        filter = {
+          event = "notify",
+          find = "No information available",
+        },
+        opts = { skip = true },
+      })
 
-    local focused = true
-    vim.api.nvim_create_autocmd("FocusGained", {
-      callback = function()
-        focused = true
-      end,
-    })
-    vim.api.nvim_create_autocmd("FocusLost", {
-      callback = function()
-        focused = false
-      end,
-    })
-    table.insert(opts.routes, 1, {
-      filter = {
-        cond = function()
-          return not focused
+      local focused = true
+      vim.api.nvim_create_autocmd("FocusGained", {
+        callback = function()
+          focused = true
         end,
-      },
-      view = "notify_send",
-      opts = { stop = false },
-    })
-
-    opts.commands = {
-      all = {
-        -- options for the message history that you get with `:Noice`
-        view = "split",
-        opts = { enter = true, format = "details" },
-        filter = {},
-      },
-    }
-
-    vim.api.nvim_create_autocmd("FileType", {
-      pattern = "markdown",
-      callback = function(event)
-        vim.schedule(function()
-          require("noice.text.markdown").keys(event.buf)
-        end)
-      end,
-    })
-
-    opts.lsp = {
-      progress = {
-        enabled = true,
-        format = {
-          { "{data.progress.percentage} ", hl_group = "Comment" },
-          { "{spinner} ", hl_group = "NoiceLspProgressSpinner" },
-          { "{data.progress.title} ", hl_group = "Comment" },
+      })
+      vim.api.nvim_create_autocmd("FocusLost", {
+        callback = function()
+          focused = false
+        end,
+      })
+      table.insert(opts.routes, 1, {
+        filter = {
+          cond = function()
+            return not focused
+          end,
         },
-        format_done = {},
-        throttle = 1000 / 30,
-        view = "mini",
-      },
-    }
+        view = "notify_send",
+        opts = { stop = false },
+      })
 
-    opts.presets = {
-      bottom_search = false, -- use a classic bottom cmdline for search
-      command_palette = false, -- position the cmdline and popupmenu together
-      long_message_to_split = true, -- long messages will be sent to a split
-      inc_rename = false, -- enables an input dialog for inc-rename.nvim
-      lsp_doc_border = true, -- add a border to hover docs and signature help
-    }
+      opts.commands = {
+        all = {
+          -- options for the message history that you get with `:Noice`
+          view = "split",
+          opts = { enter = true, format = "details" },
+          filter = {},
+        },
+      }
 
-    opts.views = {
-      mini = {
-        position = {
-          row = -1,
-          col = "100%",
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "markdown",
+        callback = function(event)
+          vim.schedule(function()
+            require("noice.text.markdown").keys(event.buf)
+          end)
+        end,
+      })
+
+      opts.lsp = {
+        progress = {
+          enabled = true,
+          format = {
+            { "{data.progress.percentage} ", hl_group = "Comment" },
+            { "{spinner} ", hl_group = "NoiceLspProgressSpinner" },
+            { "{data.progress.title} ", hl_group = "Comment" },
+          },
+          format_done = {},
+          throttle = 1000 / 30,
+          view = "mini",
         },
-      },
-      cmdline_popup = {
-        border = {
-          style = "none",
-          padding = { 1, 2 },
+      }
+
+      opts.presets = {
+        bottom_search = false, -- use a classic bottom cmdline for search
+        command_palette = false, -- position the cmdline and popupmenu together
+        long_message_to_split = true, -- long messages will be sent to a split
+        inc_rename = false, -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = true, -- add a border to hover docs and signature help
+      }
+
+      opts.views = {
+        mini = {
+          position = {
+            row = -1,
+            col = "100%",
+          },
         },
-        filter_options = {},
-        win_options = {
-          winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
+        cmdline_popup = {
+          border = {
+            style = "none",
+            padding = { 1, 2 },
+          },
+          filter_options = {},
+          win_options = {
+            winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
+          },
         },
-      },
-    }
-  end,
+      }
+    end,
+  },
 
   {
     "rcarriga/nvim-notify",
     opts = {
       timeout = 5000,
     },
+  },
+
+  -- animations
+  {
+    "echasnovski/mini.animate",
+    event = "VeryLazy",
+    opts = function(_, opts)
+      opts.scroll = {
+        enable = false,
+      }
+    end,
   },
 }
