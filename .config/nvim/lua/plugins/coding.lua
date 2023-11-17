2d627abae7a227324f957c977c87f840f826eae2
ikorason <irfanabliz914@gmail.com>
2023-11-17 13:39:57 -0500

    update

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -29,4 +29,12 @@ return {
       position = "right",
     },
   },
+
+  {
+    "nvim-cmp",
+    dependencies = { "hrsh7th/cmp-emoji" },
+    opts = function(_, opts)
+      table.insert(opts.sources, { name = "emoji" })
+    end,
+  },
 }
