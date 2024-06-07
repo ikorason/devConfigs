47b1bc096d2d4a62d1b7a0c03cbb7287cf339446
ikorason <irfanabliz914@gmail.com>
2024-06-07 17:04:27 -0400

    update treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,13 +1,16 @@
 return {
-  -- since `vim.tbl_deep_extend`, can only merge tables and not lists, the code above
-  -- would overwrite `ensure_installed` with the new value.
-  -- If you'd rather extend the default config, use the code below instead:
   {
     "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      if type(opts.ensure_installed) == "table" then
-        vim.list_extend(opts.ensure_installed, { "typescript", "tsx", "rust", "markdown", "toml", "c", "cpp" })
-      end
+    event = "VeryLazy",
+    build = ":TSUpdate",
+    opts = {
+      auto_install = true,
+      highlight = { enable = true },
+      indent = { enable = true },
+    },
+    config = function(_, opts)
+      local config = require("nvim-treesitter.configs")
+      config.setup(opts)
     end,
   },
 }
